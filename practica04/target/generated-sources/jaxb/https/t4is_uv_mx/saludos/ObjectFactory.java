4d5039f31e6d831a0237be3654fe3b23eafb81f5
RMO97 <124839099+RMO97@users.noreply.github.com>
2023-03-29 15:57:43 -0600

    Practica

---
+++ practica04/target/generated-sources/jaxb/https/t4is_uv_mx/saludos/ObjectFactory.java
@@ -0,0 +1,103 @@
+//
+// Este archivo ha sido generado por Eclipse Implementation of JAXB v3.0.0 
+// Visite https://eclipse-ee4j.github.io/jaxb-ri 
+// Todas las modificaciones realizadas en este archivo se perderán si se vuelve a compilar el esquema de origen. 
+// Generado el: 2023.03.29 a las 03:55:10 PM CST 
+//
+
+
+package https.t4is_uv_mx.saludos;
+
+import jakarta.xml.bind.annotation.XmlRegistry;
+
+
+/**
+ * This object contains factory methods for each 
+ * Java content interface and Java element interface 
+ * generated in the https.t4is_uv_mx.saludos package. 
+ * <p>An ObjectFactory allows you to programatically 
+ * construct new instances of the Java representation 
+ * for XML content. The Java representation of XML 
+ * content can consist of schema derived interfaces 
+ * and classes representing the binding of schema 
+ * type definitions, element declarations and model 
+ * groups.  Factory methods for each of these are 
+ * provided in this class.
+ * 
+ */
+@XmlRegistry
+public class ObjectFactory {
+
+
+    /**
+     * Create a new ObjectFactory that can be used to create new instances of schema derived classes for package: https.t4is_uv_mx.saludos
+     * 
+     */
+    public ObjectFactory() {
+    }
+
+    /**
+     * Create an instance of {@link SaludarRequest }
+     * 
+     */
+    public SaludarRequest createSaludarRequest() {
+        return new SaludarRequest();
+    }
+
+    /**
+     * Create an instance of {@link SaludarResponse }
+     * 
+     */
+    public SaludarResponse createSaludarResponse() {
+        return new SaludarResponse();
+    }
+
+    /**
+     * Create an instance of {@link BuscarSaludoRequest }
+     * 
+     */
+    public BuscarSaludoRequest createBuscarSaludoRequest() {
+        return new BuscarSaludoRequest();
+    }
+
+    /**
+     * Create an instance of {@link BuscarSaludoResponse }
+     * 
+     */
+    public BuscarSaludoResponse createBuscarSaludoResponse() {
+        return new BuscarSaludoResponse();
+    }
+
+    /**
+     * Create an instance of {@link EditSaludoRequest }
+     * 
+     */
+    public EditSaludoRequest createEditSaludoRequest() {
+        return new EditSaludoRequest();
+    }
+
+    /**
+     * Create an instance of {@link EditSaludoResponse }
+     * 
+     */
+    public EditSaludoResponse createEditSaludoResponse() {
+        return new EditSaludoResponse();
+    }
+
+    /**
+     * Create an instance of {@link DeleteSaludoRequest }
+     * 
+     */
+    public DeleteSaludoRequest createDeleteSaludoRequest() {
+        return new DeleteSaludoRequest();
+    }
+
+    /**
+     * Create an instance of {@link DeleteSaludoResponse }
+     * 
+     */
+    public DeleteSaludoResponse createDeleteSaludoResponse() {
+        return new DeleteSaludoResponse();
+    }
+
+}
---
+++ practica04/target/generated-sources/jaxb/https/t4is_uv_mx/saludos/package-info.java
@@ -0,0 +1,9 @@
+//
+// Este archivo ha sido generado por Eclipse Implementation of JAXB v3.0.0 
+// Visite https://eclipse-ee4j.github.io/jaxb-ri 
+// Todas las modificaciones realizadas en este archivo se perderán si se vuelve a compilar el esquema de origen. 
+// Generado el: 2023.03.29 a las 03:55:10 PM CST 
+//
+
+@jakarta.xml.bind.annotation.XmlSchema(namespace = "https://t4is.uv.mx/saludos", elementFormDefault = jakarta.xml.bind.annotation.XmlNsForm.QUALIFIED)
+package https.t4is_uv_mx.saludos;
